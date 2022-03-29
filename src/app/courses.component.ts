638d9f6d9ff72991f4d068729638e2ce83d287df
Sotiris Giannakopoulos <ecesotgian@gmail.com>
2022-03-29 20:43:25 +0300

    Get the value of input

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,11 +4,11 @@ import { CoursesService } from "./course/courses.service";
 @Component({
     selector: 'courses',
     template: `
-    <input (keyup.enter)="onKeyUp()"/>`
+    <input (keyup.enter)="onKeyUp($event)"/>`
 })
 export class CoursesComponent{
-    onKeyUp() {
-        console.log("ENTER was pressed");
+    onKeyUp($event: any) {
+        console.log($event.target.value);
     }
     
 }
