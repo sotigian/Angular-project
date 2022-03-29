3186f01442f42e54b5505f6109838cf66b2bdf58
Sotiris Giannakopoulos <ecesotgian@gmail.com>
2022-03-29 18:47:56 +0300

    method getTitle

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ "Title: " + title}}</h2>'
+    template: '<h2>{{ "Title: " + getTitle()}}</h2>'
 })
 export class CoursesComponent{
     title = "List of Courses";
+    getTitle() {
+        return this.title;
+    }
 }
