d4423940c23c72d0aa62fa0798e4efb561add4b4
Sotiris Giannakopoulos <ecesotgian@gmail.com>
2022-03-29 18:45:17 +0300

    Templates with fields (string interpolation)

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ "Title: " + title}}</h2>'
 })
 export class CoursesComponent{
-
+    title = "List of Courses";
 }
