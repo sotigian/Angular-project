ae80f9d428d7b0807cf84b67be4c7180e8aa5826
Sotiris Giannakopoulos <ecesotgian@gmail.com>
2022-03-30 18:26:40 +0300

    Pipes Intro

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,13 +3,19 @@ import { CoursesService } from "./course/courses.service";
 
 @Component({
     selector: 'courses',
-    template: `
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>`
+    template: `{{course.title | uppercase }} <br>
+    {{course.students | number}} <br>
+    {{course.rating | number:'2.2-3'}}<br>
+    {{course.price | currency: "EUR"}}<br>
+    {{course.releasedDate | date: 'shortDate'}}`
 })
 export class CoursesComponent{
-    email ="me@gmail.com" 
-    onKeyUp() {
-        console.log(this.email);
+    course = {
+        title: "The complete Angular course",
+        students: 30123,
+        rating: 4.9745,
+        price:190.95,
+        releasedDate: new Date(2016,2,3)
     }
     
 }
