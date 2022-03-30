c230d191ff519b1d548990af476ddf163823731a
Sotiris Giannakopoulos <ecesotgian@gmail.com>
2022-03-30 19:55:51 +0300

    Load Courses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [
-    {id:1, title: "Java"},
-    {id:2, title: "C+"},
-    {id:3, title: "Python"},
-    {id:4, title: "Javascript"}
-  ];
-
-  onAdd() {
-    this.courses.push({id:5, title: "Another Course"});
-  }
-
-  onRemove(course: any) {
-    let index = this.courses.indexOf(course);
-    this.courses.splice(index,1);
-  }
-
-  onChange(course: any) {
-    course.title += "updated";
+  courses: any;
+  loadCourses(){
+    this.courses = [
+      {id:1, title: "Java"},
+      {id:2, title: "C+"},
+      {id:3, title: "Python"},
+      {id:4, title: "Javascript"}
+    ];
   }
 }
