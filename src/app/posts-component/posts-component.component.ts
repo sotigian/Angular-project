990a4b7d29cec8c6ef2620c066e9d902101e5865
Sotiris Giannakopoulos <ecesotgian@gmail.com>
2022-03-31 19:33:55 +0300

    Http service creating data

--- src/app/posts-component/posts-component.component.ts
+++ src/app/posts-component/posts-component.component.ts
@@ -8,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponentComponent {
   posts: any
+  url = 'https://jsonplaceholder.typicode.com/posts'
 
   constructor(private http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts').subscribe(response =>{
+    http.get(this.url).subscribe(response =>{
       this.posts = response;
     })
    }
  
+   createPost(input: HTMLInputElement) {
+    let post = {title: input.value};
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        this.posts.unshift(post);
+        console.log(response);
+      });
+  }
   
 }
