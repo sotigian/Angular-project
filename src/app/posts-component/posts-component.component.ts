be10fa51256a3e72820b808f783fdca2f1efe190
Sotiris Giannakopoulos <ecesotgian@gmail.com>
2022-03-31 20:01:40 +0300

    Http Services: Updating Data

--- src/app/posts-component/posts-component.component.ts
+++ src/app/posts-component/posts-component.component.ts
@@ -25,5 +25,12 @@ export class PostsComponentComponent {
         console.log(response);
       });
   }
+
+  updatePost(post: any) {
+    this.http.patch(`${this.url}/${post.id}`, JSON.stringify({isRead:true}))
+    .subscribe(response => {
+      console.log(response);
+    });
+  }
   
 }
