d892bf6d90756735f5995e401ab6ba15e65bca2e
Sotiris Giannakopoulos <ecesotgian@gmail.com>
2022-04-01 15:56:51 +0300

    Fixed Final Architecture with inheritance

--- src/app/posts-component/posts-component.component.ts
+++ src/app/posts-component/posts-component.component.ts
@@ -1,29 +1,53 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'posts',
   templateUrl: './posts-component.component.html',
   styleUrls: ['./posts-component.component.css']
 })
-export class PostsComponentComponent {
+export class PostsComponentComponent implements OnInit {
   posts: any
-  url = 'https://jsonplaceholder.typicode.com/posts'
+ 
 
-  constructor(private http: HttpClient) {
-    http.get(this.url).subscribe(response =>{
+  constructor(private service: PostService) {
+    
+  
+  }
+  ngOnInit(): void {
+    this.service.getAll()
+    .subscribe(response => {
       this.posts = response;
     })
-   }
- 
-   createPost(input: HTMLInputElement) {
-    let post = {title: input.value};
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = {id:200, title: input.value };
     input.value = '';
-    this.http.post(this.url, JSON.stringify(post))
+    this.service.create(post)
       .subscribe(response => {
         this.posts.unshift(post);
         console.log(response);
       });
   }
-  
+
+  updatePost(post: any) {
+    this.service.update(post)
+      .subscribe(response => {
+        console.log(response);
+      });
+
+  }
+
+  deletePost(post: any) {
+    this.service.delete(post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+        console.log(response);
+      })
+  }
+
+
 }
